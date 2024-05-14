33484ac26b74fb9a19c4e4b8309ff570f17c5416
Christopher <92882782+Krostofer@users.noreply.github.com>
2024-05-14 09:14:07 -0400

    Initial commit

---
+++ com/example/SelectSort.java
@@ -0,0 +1,28 @@
+
+package com.example;
+
+/**
+ * Change me.
+ *
+ * @author Christopher Di Bert
+ * @version 1.0
+ * @since 2024-05-14
+ */
+
+// SelectSort class
+public final class SelectSort {
+
+    /** Private constructor to prevent instantiation. */
+    private SelectSort() {
+        throw new UnsupportedOperationException("Cannot instantiate");
+    }
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world");
+    }
+}
